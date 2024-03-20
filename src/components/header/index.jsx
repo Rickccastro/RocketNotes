dd5545c5e6a19b9572281179ae863c236762d266
ricardo <ricardoparmezancastro.dev@gmail.com>
2024-03-20 10:16:28 -0300

    Update Notes&Tags/Delete/FinishProject

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,9 +2,18 @@ import { Container,Profile,Logout} from "./style";
 import {RiShutDownLine} from'react-icons/ri'
 import { api } from "../../services/api";
 import { useAuth } from "../../hooks/auth";
+import avatarPlacehoalder from '../../assets/avatar_placeholder.svg'
+import { useNavigate } from "react-router-dom";
 
 export function Header(){
-    const {signOut,user}=useAuth()
+    const {signOut,user}=useAuth();
+    const navigate=useNavigate();
+
+    function handleSignOut(){
+        navigate("/");
+        signOut();
+        
+    }
 
     const avatarUrl=user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlacehoalder
 
@@ -17,7 +26,7 @@ export function Header(){
                     <strong>{user.name}</strong>
                 </div>
             </Profile>
-            <Logout onClick={signOut}>
+            <Logout onClick={handleSignOut}>
                 <RiShutDownLine />
             </Logout>
         </Container>
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,3 +1,4 @@
+import { useState,useEffect } from 'react'
 import { FiPlus,FiSearch } from 'react-icons/fi'
 
 import {Container,Brand,Menu,Search,Content,NewNote} from './style'
@@ -12,10 +13,59 @@ import { Note } from '../../components/Note'
 import {Section} from'../../components/section'
 
 import {ButtonText} from '../../components/buttonText'
+import { api } from '../../services/api'
+import { useNavigate } from 'react-router-dom'
 
 
 export function Home(){
 
+    const [search,setSearch]=useState("");
+    const [notes,setNotes]=useState([]);
+    const [tags,setNewTags]=useState([]);
+    const [tagsSelected,setTagsSelected]=useState([]);
+
+
+    const navigate=useNavigate()
+
+    function handleTagSelected(tagName){
+        if(tagName==="all"){
+             return setTagsSelected([])
+        }
+        const alreadySelected=tagsSelected.includes(tagName)
+
+        if(alreadySelected){
+            /**seleciona todas as tags q Não está sendo selecionada e atualiza  */
+            const filteredTags=tagsSelected.filter(tag=>tag !==tagName)
+            setTagsSelected(filteredTags)
+        }else{
+            setTagsSelected(prevState => [...prevState, tagName]);
+        }
+    }
+
+    function handleDetails(id){
+        navigate(`/details/${id}`)
+
+    }
+    
+    /**carrega apenas 1 vez */
+    useEffect(()=>{
+        async function fetchTags(){
+            const response = await api.get("/tags")
+            setNewTags(response.data)
+        }
+        fetchTags()
+    },[])
+
+    /**carrega quando tagsSelected ou search mudar */
+    useEffect(()=>{
+
+        async function fetchNotes(){
+        const response = await api.get(`/notes?title=${search}&tags=${tagsSelected}`)
+            setNotes(response.data)
+        }
+        fetchNotes();
+
+    },[tagsSelected,search])
     return(
         <Container>
         <Brand>
@@ -25,25 +75,49 @@ export function Home(){
         <Header/>
 
         <Menu>
-            <li><ButtonText title="Todos" isActive/></li>
-            <li><ButtonText title="React"/></li>
-            <li><ButtonText title="Node"/></li>
+       
+        <li>
+            <ButtonText 
+            title="Todos" 
+            onClick={()=>handleTagSelected("all")}
+            isActive={tagsSelected.length === 0}
+            />
+        </li>
+
+            {
+                tags && tags.map(tag=>(
+
+                    <li key={String (tag.id)}> 
+                        <ButtonText 
+                        title={tag.name}
+                        onClick={()=>handleTagSelected(tag.name)}
+                        isActive={tagsSelected.includes(tag.name)}
+                        />
+
+                    </li>
+                ))
+            }
+
         </Menu>
 
         <Search>
-        <Input placeholder="Pesquisar pelo Titulo" icon={FiSearch}/>
+            <Input 
+            onChange={(e)=>setSearch(e.target.value)}
+            placeholder="Pesquisar pelo Titulo"
+            icon={FiSearch}/>
         </Search>
 
         <Content>
             <Section title="Minhas Notas">
-
-             <Note
-              data={{title:'React',
-              tags:[
-                {id:'1',name:'react'},
-                {id:'2',name:'library'}
-                  ] 
-             }}/>
+              {
+                notes.map(note=>(
+                    <Note
+                     key={String(note.id)}
+                     data={note}
+                     onClick={()=>handleDetails(note.id)}
+                    />
+                ))
+              }  
             </Section>
 
         </Content>
